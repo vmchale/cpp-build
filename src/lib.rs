9e39e5c48a3cada788129a74dc4671fe2e564193
Vanessa McHale <vamchale@gmail.com>
2020-10-08 18:27:52 -0500

    doc-y docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,28 @@ use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use walkdir::WalkDir;
 
+pub enum CCompiler {
+    GCC,
+    ICC,
+    Clang,
+    PGCC,
+}
+
+/// Preprocess using [cpphs](https://archives.haskell.org/projects.haskell.org/cpphs/).
+pub fn pp_cpphs(fp: &Path, out: &Path) -> () {
+    let os_p = fp.as_os_str();
+    let out_p = out.as_os_str();
+    let _ = Command::new("cpphs")
+        .args(&[os_p, out_p])
+        .output()
+        .expect("call to C preprocessor failed");
+}
+
 /// should work with pgcc, icc, clang, gcc...
 pub fn pp_cc(cc: &str, fp: &Path, out: &Path) -> () {
     let os_p = fp.as_os_str();
     let cpp_res = Command::new(cc)
-        .args(&[OsStr::new("-E"), OsStr::new("-x"), OsStr::new("c"), os_p])
+        .args(&[OsStr::new("-E"), OsStr::new("-x"), OsStr::new("c"), os_p]) // FIXME: don't pass -x c for pgcc (clang, gcc need it)
         .stdout(Stdio::piped())
         .output()
         .expect("call to C preprocessor failed");
@@ -19,6 +36,7 @@ pub fn pp_cc(cc: &str, fp: &Path, out: &Path) -> () {
 }
 // also I should allow users to pass -I flags lol (and -D) like cc crate?
 
+// maybe get a .rs file name from a .cpprs file
 fn as_rs(fp: &Path) -> Option<PathBuf> {
     let maybe_ext = fp.extension();
     maybe_ext.and_then({
@@ -35,11 +53,12 @@ fn as_rs(fp: &Path) -> Option<PathBuf> {
     })
 }
 
-pub fn walk_src(cc: &str) -> () {
-    walk_dir(cc, "src");
+pub fn walk_src_preprocess(cc: &str) -> () {
+    walk_preprocess(cc, "src");
 }
 
-fn walk_dir<P: AsRef<Path>>(cc: &str, dir: P) -> () {
+/// This function walks a given directory
+pub fn walk_preprocess<P: AsRef<Path>>(cc: &str, dir: P) -> () {
     for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
         let mdo = as_rs(entry.path());
         match mdo {
