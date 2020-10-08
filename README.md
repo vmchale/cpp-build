e266649923b87ec0c4b55c0025de5d5079ac995b
Vanessa McHale <vamchale@gmail.com>
2020-10-08 18:51:58 -0500

    Works most-ish

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # cpp-rs
 
 Put this in your `build.rs` to use the C pre-processor with Rust.
+
+## Example Use
+
+```rust
+fn main() {
+    walk_dir(CCompiler::GCC, "src")
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,33 @@ pub enum CCompiler {
     PGCC,
 }
 
+fn ccompiler(cc: &CCompiler) -> String {
+    match cc {
+        CCompiler::GCC => "gcc",
+        CCompiler::ICC => "icc",
+        CCompiler::Clang => "clang",
+        CCompiler::PGCC => "pgcc",
+    }
+    .to_owned()
+}
+
+fn cflags(cc: &CCompiler) -> Vec<&OsStr> {
+    match cc {
+        CCompiler::GCC => vec!["-E", "-x", "c"]
+            .into_iter()
+            .map(|x| OsStr::new(x))
+            .collect(),
+        CCompiler::ICC => vec!["-E"].into_iter().map(|x| OsStr::new(x)).collect(),
+        CCompiler::PGCC => vec!["-E"].into_iter().map(|x| OsStr::new(x)).collect(),
+        CCompiler::Clang => vec!["-E", "-x", "c"]
+            .into_iter()
+            .map(|x| OsStr::new(x))
+            .collect(),
+    }
+}
+
 /// Preprocess using [cpphs](https://archives.haskell.org/projects.haskell.org/cpphs/).
-pub fn pp_cpphs(fp: &Path, out: &Path) -> () {
+pub fn pp_cpphs(fp: &Path, out: &Path) {
     let os_p = fp.as_os_str();
     let out_p = out.as_os_str();
     let _ = Command::new("cpphs")
@@ -22,11 +47,13 @@ pub fn pp_cpphs(fp: &Path, out: &Path) -> () {
         .expect("call to C preprocessor failed");
 }
 
-/// should work with pgcc, icc, clang, gcc...
-pub fn pp_cc(cc: &str, fp: &Path, out: &Path) -> () {
+pub fn pp_cc(cc: &CCompiler, fp: &Path, out: &Path) {
     let os_p = fp.as_os_str();
-    let cpp_res = Command::new(cc)
-        .args(&[OsStr::new("-E"), OsStr::new("-x"), OsStr::new("c"), os_p]) // FIXME: don't pass -x c for pgcc (clang, gcc need it)
+    let mut args0 = cflags(cc);
+    args0.push(os_p);
+    // FIXME: borrow?
+    let cpp_res = Command::new(ccompiler(cc))
+        .args(args0) // FIXME: don't pass -x c for pgcc (clang, gcc need it)
         .stdout(Stdio::piped())
         .output()
         .expect("call to C preprocessor failed");
@@ -46,24 +73,24 @@ fn as_rs(fp: &Path) -> Option<PathBuf> {
                 let mut return_pbuf = PathBuf::from(fp.parent().unwrap());
                 return_pbuf.push(main_fp);
                 return_pbuf.set_extension("rs");
-                return Some(return_pbuf);
+                Some(return_pbuf)
             }
-            _ => return None,
+            _ => None,
         }
     })
 }
 
-pub fn walk_src_preprocess(cc: &str) -> () {
+pub fn walk_src_preprocess(cc: CCompiler) {
     walk_preprocess(cc, "src");
 }
 
 /// This function walks a given directory
-pub fn walk_preprocess<P: AsRef<Path>>(cc: &str, dir: P) -> () {
+pub fn walk_preprocess<P: AsRef<Path>>(cc: CCompiler, dir: P) {
     for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
         let mdo = as_rs(entry.path());
         match mdo {
             None => (),
-            Some(out_fp) => pp_cc(cc, entry.path(), &out_fp),
+            Some(out_fp) => pp_cc(&cc, entry.path(), &out_fp),
         }
     }
 }
@@ -71,9 +98,15 @@ pub fn walk_preprocess<P: AsRef<Path>>(cc: &str, dir: P) -> () {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn walk_test() {
-        walk_dir("gcc", "test/demo")
+        let res = fs::remove_file("test/demo/test.rs");
+        match res {
+            Ok(_) => (),
+            Err(_) => (),
+        };
+        walk_preprocess(CCompiler::GCC, "test/demo")
     }
 }
